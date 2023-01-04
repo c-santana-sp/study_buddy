2a3e99ede2204252fed408755696747f2b8286b1
c-santana-sp <eduardomarquesdesantana@gmail.com>
2023-01-03 23:33:50 -0300

    feat: implementing Room, Topic and Message models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE, SET_NULL
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    room = models.ForeignKey(Room, on_delete=CASCADE)
+    body = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
